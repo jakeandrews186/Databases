4aad32fd6290d7fd3a3f96d0df323b6715cef625
Jeong-Hyun Boo <jeong-hyunboo@u.boisestate.edu>
2020-04-19 17:28:53 -0600

    sql

---
+++ GroupProject.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE GroupProject;
+
+CREATE TABLE Class 
+(
+	c_id INT NOT NULL AUTO_INCREMENT,
+    c_number VARCHAR(5) NOT NULL,
+    term VARCHAR(4) NOT NULL,
+    sec_number INT AUTO_INCREMENT NOT NULL,
+    description VARCHAR(100),
+    PRIMARY KEY (c_id)
+);
+
+CREATE TABLE Category 
+(
+	cg_id INT NOT NULL,
+    c_id INT NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    weight DECIMAL(2,2) NOT NULL,
+    PRIMARY KEY(cg_id),
+    FOREIGN KEY (c_id) REFERENCES Class(c_id)
+);
+
+CREATE TABLE Student
+(
+	s_id INT NOT NULL,
+    user_name VARCHAR(20) NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (s_id)
+);
+
+CREATE TABLE Assignment 
+(
+    a_id INT NOT NULL,
+    cg_id INT NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    description VARCHAR(100), 
+    point_value INT NOT NULL,
+    s_id INT NOT NULL,
+    grade VARCHAR(2) NOT NULL,
+    PRIMARY KEY (a_id),
+    FOREIGN KEY (cg_id) REFERENCES Category(cg_id),
+    FOREIGN KEY (s_id) REFERENCES Student(s_id)
+); 
+
+CREATE TABLE Grade
+(
+	g_id INT NOT NULL, 
+    cg_id INT NOT NULL,
+    s_id INT NOT NULL,
+    c_id INT NOT NULL,
+    a_id INT NOT NULL, 
+    grade VARCHAR(2) NOT NULL, 
+    PRIMARY KEY (g_id),
+    FOREIGN KEY (cg_id) REFERENCES Category(cg_id),
+    FOREIGN KEY (c_id) REFERENCES Class(c_id),
+    FOREIGN KEY (a_id) REFERENCES Assignment(a_id)
+);
+
+DELIMITER $$
+CREATE PROCEDURE createClass(IN c_number VARCHAR(5), term VARCHAR(4),  description VARCHAR(100), sec_number INT)
+BEGIN
+	INSERT INTO Class (C_number, Term, Description, Sec_number) VALUES (c_number, term, description, sec_number);
+END $$
+
+    
+    
