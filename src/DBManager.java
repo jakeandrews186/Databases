3936c447fcdc4a9face174c945944c5a5d1b9891
JEONG-HYUN BOO <jeong-hyunboo@u.boisestate.edu>
2020-04-29 11:47:17 -0600

    moved methods over from Project

--- src/DBManager.java
+++ src/DBManager.java
@@ -29,20 +29,21 @@ public class DBManager {
 			{
 			case "new-class":
 				newClass(inputs[1], inputs[2], inputs[3], inputs[4]);
-			
+			case "list-classes":
+				listClass();
 			case "select-class":
 				if (inputs.length == 2)
 				{
-					selectClass(inputs[1], "", "");
+					selectClass1(inputs[1]);
 				}
 				else if (inputs.length == 3)
 				{
-					selectClass(inputs[1], inputs[2], "");
+					selectClass2(inputs[1], inputs[2]);
 
 				}
 				else if (inputs.length == 4)
 				{
-					selectClass(inputs[1], inputs[2], inputs[3]);
+					selectClass3(inputs[1], inputs[2], inputs[3]);
 
 				}
 			case "show-class":
@@ -156,7 +157,8 @@ public class DBManager {
 		the specified student does not exist, report an error
 	 * @param username
 	 */
-	private static void addStudent(String username) {
+	private static void addStudent(String username) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
@@ -171,7 +173,8 @@ public class DBManager {
 	 * @param last
 	 * @param first
 	 */
-	private static void addStudent(String username, String studentID, String last, String first) {
+	private static void addStudent(String username, String studentID, String last, String first) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
@@ -184,16 +187,48 @@ public class DBManager {
 	 * @param description
 	 * @param points
 	 */
-	private static void addAssignment(String name, String category, String description, String points) {
-		// TODO Auto-generated method stub
+	private static void addAssignment(String name, String category, String description, String points) 
+	{
+		String query = "CALL addAssignment(?, ?, ?, ?);";
+		try 
+		{
+			CallableStatement stmt = conn.prepareCall(query);
+			stmt.setString(1, name);
+			stmt.setString(2, category);
+			stmt.setString(3, description);
+			stmt.setInt(4, Integer.parseInt(points));
+
+			ResultSet rs = stmt.executeQuery();
+			conn.close();
+		} catch (SQLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 		
 	}
 
 	/**
 	 * list the assignments with their point values, grouped by category
 	 */
-	private static void showAssignment() {
-		// TODO Auto-generated method stub
+	private static void showAssignment() 
+	{
+		String query = "CALL showAssignment();";
+		try
+		{
+			CallableStatement stmt = conn.prepareCall(query);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next())
+			{
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getInt(3) + "\n"); 
+			}
+			conn.close();
+		}
+		catch (SQLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
@@ -202,17 +237,46 @@ public class DBManager {
 	 * @param name
 	 * @param weight
 	 */
-	private static void addCategory(String name, String weight) {
-		// TODO Auto-generated method stub
+	private static void addCategory(String name, String weight) 
+	{
+		String query = "CALL addCategory(?, ?);";
+		try 
+		{
+			CallableStatement stmt = conn.prepareCall(query);
+			stmt.setString(1, name);
+			stmt.setLong(2, Long.parseLong(weight));
+			ResultSet rs = stmt.executeQuery();
+			conn.close();
+		} 
+		catch (SQLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
 	/**
 	 * list the categories with their weights
 	 */
-	private static void showCategories() {
-		// TODO Auto-generated method stub
-		
+	private static void showCategories() 
+	{
+		String query = "CALL showCategories();";
+		try
+		{
+			CallableStatement stmt = conn.prepareCall(query);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next())
+			{
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getLong(3) + "\n"); 
+			}
+			conn.close();
+		}
+		catch (SQLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 	}
 
 	/**
@@ -237,6 +301,72 @@ public class DBManager {
 			e.printStackTrace();
 		}			
 	}
+	
+	/**
+	 * select a new class from the database
+	 * @param code
+	 * @param term
+	 */
+	private static void selectClass1(String code) 
+	{
+		String query = "CALL select_class1(?);";
+		try {
+			CallableStatement stmt = conn.prepareCall(query);
+			stmt.setString(1, code);	
+			ResultSet rs = stmt.executeQuery();
+			if(!rs.first())
+			{
+				System.out.println("Command failed as there are multiple sections.");
+			}
+			else
+			{
+	            while (rs.next()) 
+	            {
+	                System.out.println("Selected Class: " + rs.getString("description"));
+	                selectedClass = rs.getInt("c_id");
+	            }
+			}
+			conn.close();
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}			
+	}
+	
+	/**
+	 * select a new class from the database
+	 * @param code
+	 * @param term
+	 */
+	private static void selectClass2(String code, String term) 
+	{
+		String query = "CALL select_class2(?, ?);";
+		try 
+		{
+			CallableStatement stmt = conn.prepareCall(query);
+			stmt.setString(1, code);
+			stmt.setString(2, term);	
+			ResultSet rs = stmt.executeQuery();
+			if(!rs.first())
+			{
+				System.out.println("Command failed as there are multiple sections.");
+			}
+			else
+			{
+	            while (rs.next()) 
+	            {
+	                System.out.println("Selected Class: " + rs.getString("description"));
+	                selectedClass = rs.getInt("c_id");
+	            }
+			}
+			conn.close();
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}			
+	}
 
 	/**
 	 * select a new class from the database
@@ -244,8 +374,9 @@ public class DBManager {
 	 * @param term
 	 * @param section
 	 */
-	private static void selectClass(String code, String term, String section) {
-		String query = "{CALL select_class3(?, ?, ?)}";
+	private static void selectClass3(String code, String term, String section) 
+	{
+		String query = "CALL select_class3(?, ?, ?);";
 		try {
 			CallableStatement stmt = conn.prepareCall(query);
 			stmt.setString(1, code);
@@ -260,7 +391,29 @@ public class DBManager {
 			e.printStackTrace();
 		}			
 	}
-
+	/**
+	 * Lists classes with the number of students in each class 
+	 */
+	private static void listClass()
+	{
+		String query = "CALL listClass();";
+		try
+		{
+			CallableStatement stmt = conn.prepareCall(query);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next())
+			{
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + "\n"); 
+			}
+			conn.close();
+		}
+		catch (SQLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+	
 	/**
 	 * Creates a new class
 	 * @param code - the class code
@@ -268,9 +421,11 @@ public class DBManager {
 	 * @param section - the section of the class
 	 * @param title - the title of the class
 	 */
-	private static void newClass(String code, String term, String section, String title) {
-		String query = "{CALL create_class(?, ?, ?, ?)}";
-		try {
+	private static void newClass(String code, String term, String section, String title) 
+	{
+		String query = "CALL createClass(?, ?, ?, ?);";
+		try 
+		{
 			CallableStatement stmt = conn.prepareCall(query);
 			stmt.setString(1, code);
 			stmt.setString(2, term);
@@ -278,7 +433,9 @@ public class DBManager {
 			stmt.setString(4, title);
 
 			ResultSet rs = stmt.executeQuery();
-		} catch (SQLException e) {
+			conn.close();
+		} catch (SQLException e) 
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}		
